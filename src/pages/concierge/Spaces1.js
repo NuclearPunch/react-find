dcbc79768eab1205589f9415c005a0ea3903acb1
pjh <skwjdg2@gmail.com>
2019-01-18 18:49:03 +0900

    update util

--- src/pages/concierge/Spaces1.js
+++ src/pages/concierge/Spaces1.js
@@ -6,7 +6,7 @@ import Util from  './../../lib/Util';
 
 
 const ContentBox = styled.div`
-  width: 1064px;
+  width: 798px;
   height: 330px;
   margin: 0 auto;
   margin-top: 170px;
@@ -19,6 +19,15 @@ const BttonBox = styled.div`
   margin: 0 auto;
   margin-top: 130px;
 `;
+
+const Test = styled(Link)`
+  width: 456px;
+  height: 60px;
+  margin: 0 auto;
+  margin-top: 130px;
+`;
+
+
 
 class Spaces1 extends Component {
    
@@ -55,12 +64,11 @@ class Spaces1 extends Component {
                     <ConciergeCard id={this.state.card[0].id} title={this.state.card[0].title} subTitle={this.state.card[0].subTitle} img={this.state.card[0].imgSrc} selected={this.state.card[0].selected} onClick={(e) => this.handleActiveChange(this.state.card[0].id, e)} type={"M"}/>
                     <ConciergeCard id={this.state.card[1].id} title={this.state.card[1].title} subTitle={this.state.card[1].subTitle} img={this.state.card[1].imgSrc} selected={this.state.card[1].selected} onClick={(e) => this.handleActiveChange(this.state.card[1].id, e)} type={"M"}/>
                     <ConciergeCard id={this.state.card[2].id} title={this.state.card[2].title} subTitle={this.state.card[2].subTitle} img={this.state.card[2].imgSrc} selected={this.state.card[2].selected} onClick={(e) => this.handleActiveChange(this.state.card[2].id, e)} type={"M"}/>
-                    <ConciergeCard id={this.state.card[3].id} title={this.state.card[3].title} subTitle={this.state.card[3].subTitle} img={this.state.card[3].imgSrc} selected={this.state.card[3].selected} onClick={(e) => this.handleActiveChange(this.state.card[3].id, e)} type={"M"}/>
                 </ContentBox>
                 <BttonBox>
                     <Button >이전으로</Button>
                    
-                    <Button active={this.state.active} >다음으로<NavLink  to="/concierge/spaces2"></NavLink></Button>
+                    <Button active={this.state.active} ><Test2  to="/concierge/spaces2">다음으로</Test2></Button>
                 </BttonBox>
             </div>
 
